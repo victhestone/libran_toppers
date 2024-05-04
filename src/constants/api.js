5c6a72d69942063d491dc10bda3e78a0bc9f8b56
victhestone <victor.vansteenkiste1a8@gmail.com>
2024-05-04 18:05:39 +0200

    fixes: prd

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost:8080';
-// export const URL = 'https://libra-toppers-server.azurewebsites.net';
+/* export const URL = 'http://localhost:8080'; */
+export const URL = 'https://libra-toppers-server.azurewebsites.net';
